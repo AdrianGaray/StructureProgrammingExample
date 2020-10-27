d68ba8526b44232f01dd406fff0f28c0252fb894
Adrian <c.adga@tessa.com.uy>
2020-10-26 21:24:36 -0300

    Se modifica el archivo program

--- StructureProgrammingExample/Program.cs
+++ StructureProgrammingExample/Program.cs
@@ -10,37 +10,46 @@ namespace StructureProgrammingExample
     {
         static void Main(string[] args)
         {
-            int operation;
-            int firstdata;
-            int seconddata;
+            double operation;
+            double firstdata;
+            double seconddata;
 
-            Console.WriteLine("Operacion Calculadora:");
-            Console.WriteLine("1. Suma");
-            Console.WriteLine("2. Resta");
-            operation = GetIntegerDataFromUser("Seleccionar operacion a ejecutar");
-
-            switch (operation)
+            bool isValid = false;
+            while (!isValid)
             {
-                case 1:
-                    // Suma
-                    firstdata = GetIntegerDataFromUser("Ingrese el primer parametro, debe ser entero:");
-                    seconddata = GetIntegerDataFromUser("Ingrese el segundo parametro, debe ser entero:");
-                    // string con Interpolacion
-                    Console.WriteLine($"El resultado de la Suma {firstdata} + {seconddata} = {firstdata + seconddata}");
-                    break;
-                case 2:
-                    // Resta
-                    firstdata = GetIntegerDataFromUser("Ingrese el primer parametro, debe ser entero:");
-                    seconddata = GetIntegerDataFromUser("Ingrese el segundo parametro, debe ser entero:");
-                    // string con Interpolacion
-                    Console.WriteLine($"El resultado de la Resta {firstdata} + {seconddata} = {firstdata - seconddata}");
-                    break;
-                default:
-                    Console.WriteLine("La opcion seleccionada no existe.");
-                    break;
+                Console.WriteLine("Operacion Calculadora:");
+                Console.WriteLine("1. Suma");
+                Console.WriteLine("2. Resta");
+                Console.WriteLine("3. Salir del Programa");
+                operation = GetDoubleDataFromUser("Seleccionar operacion a ejecutar");
+
+                switch (operation)
+                {
+                    case 1:
+                        // Suma
+                        firstdata = GetDoubleDataFromUser("Ingrese el primer parametro, debe ser entero:");
+                        seconddata = GetDoubleDataFromUser("Ingrese el segundo parametro, debe ser entero:");
+                        // string con Interpolacion
+                        Console.WriteLine($"El resultado de la Suma {firstdata} + {seconddata} = {firstdata + seconddata}");
+                        break;
+                    case 2:
+                        // Resta
+                        firstdata = GetDoubleDataFromUser("Ingrese el primer parametro, debe ser entero:");
+                        seconddata = GetDoubleDataFromUser("Ingrese el segundo parametro, debe ser entero:");
+                        // string con Interpolacion
+                        Console.WriteLine($"El resultado de la Resta {firstdata} + {seconddata} = {firstdata - seconddata}");
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("La opcion seleccionada no existe.");
+                        break;
+                }
+                Console.WriteLine("Presione cualquier tecla para seguir operando");
+                Console.ReadKey();
+                Console.Clear();                
             }
-            Console.WriteLine("Presione cualquier tecla para salir...");
-            Console.Read();
         }
 
         private static int GetIntegerDataFromUser(string message)
@@ -55,6 +64,30 @@ namespace StructureProgrammingExample
                 userData = Console.ReadLine();
 
                 if (!int.TryParse(userData, out data))
+                {
+                    Console.WriteLine("El dato que no es valido. Vuelva a intentar de nuevo");
+                }
+                else
+                {
+                    isDataValid = true;
+                }
+            }
+
+            return data;
+        }
+
+        public static double GetDoubleDataFromUser(string message) 
+        {
+            string userData;
+            double data = 0;
+            bool isDataValid = false;
+
+            while (!isDataValid)
+            {
+                Console.WriteLine(message);
+                userData = Console.ReadLine();
+
+                if (!double.TryParse(userData, out data))
                 {
                     Console.WriteLine("El dato que no es valido. Vuelva a intentar de nuevo");
                 }
